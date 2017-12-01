f16eaef25311b4ed18dc6d6e4da575f1543ed88e
Nancy Ho <nancyho@mit.edu>
2017-12-01 16:18:36 -0500

    updated identities

---
+++ insertingData.sql
@@ -0,0 +1,5 @@
+insert into account values ("nancyho", "PM");
+insert into account values ("petramanudom", "SWE");
+
+insert into identities values ("East Asian", "nancyho");
+insert into identities values ("Southeast Asian", "petramanudom");
