9211a8d414464e8f9c9644142e8d9bdbf6c43ad2
Nancy Ho <nancyho@mit.edu>
2017-12-06 23:57:57 -0500

    fixed ddl and inserting data

--- public_html/ideal.sql
+++ public_html/ideal.sql
@@ -1,25 +1,31 @@
 use weddit_db;
 drop table if exists terms;
-drop table if exists companies;
 drop table if exists reviews;
 drop table if exists identities;
+drop table if exists companies;
 drop table if exists account;
 
-create table account(
+create table account (
 	accountName varchar(20) primary key,
 	position varchar(20)
 	)
 
 ENGINE = InnoDB;
 
-create table identities(
-	identity ENUM("East Asian", "Southeast Asian", "Cis-woman",
+create table companies (
+	companyName varchar(20) primary key
+	)
+
+ENGINE = InnoDB;
+
+create table identities (
+	identity ENUM ("East Asian", "Southeast Asian", "Cis-woman",
 		"Cis-man", "Non-binary","Genderfluid", "Trans", "Trans-Woman", "Trans-Man",
 		"White", "Black", "Latinx", "South Asian", "Pacific Islander", "Low income",
 		"First Generation College Student", "Immigrant", "Disabled", "Able-bodied", "Undocumented",
 		"Fat", "Indigenous", "Gay", "Heterosexual", "Lesbian", "Bisexual", "Queer",
-		"Asexual", "Aromantic", "Multi-racial"),
-	accountName varchar(20) not null,
+		"Asexual", "Aromantic", "Multi-racial") not null,
+	accountName varchar(20),
 	primary key (identity, accountName),
 	foreign key (accountName) references account(accountName) on delete restrict
 	)
@@ -32,19 +38,14 @@ create table reviews (
 	reviewText LONGBLOB not null,
 	sentiment ENUM("Positive", "Negative", "Neutral") not null,
 	salary int unsigned,
+	companyName varchar(20),
 	primary key (reviewID),
-	foreign key (accountName) references account(accountName) on delete restrict
+	foreign key (accountName) references account(accountName) on delete restrict,
+	foreign key (companyName) references companies(companyName) on delete restrict
 	)
 
 ENGINE = InnoDB;
 
-create table companies (
-	name varchar(20) primary key,
-	reviewID int not null AUTO_INCREMENT,
-	foreign key (reviewID) references reviews(reviewID) on delete restrict
-	)
-
-ENGINE = InnoDB;
 
 create table terms (
 	term varchar(20) not null,
--- public_html/insertingData.sql
+++ public_html/insertingData.sql
@@ -6,8 +6,9 @@ insert into identities values ("Southeast Asian", "petramanudom");
 insert into identities values ("Cis-woman", "nancyho");
 insert into identities values ("Non-binary", "petramanudom");
 
-insert into reviews values (null, "nancyho", "Very large corporate company, Majority white company, deals with diversity issues similar to many other corporate companies", "Neutral", 70000);
+insert into companies values ("Capital LLC");
 
-insert into companies values ("Capital LLC", null);
+insert into reviews values (null, "nancyho", "Very large corporate company, Majority white company, deals with diversity issues similar to many other corporate companies", "Neutral", 70000, "Capital LLC");
+insert into reviews values (null, "petramanudom", "This company was ok. I think that this company is a good place to be when you are first starting out, but not alot of space for growth. Probably becasue theyre racist tbqh", "Neutral", 80000, "Capital LLC");
 
 insert into terms values ("corporate", null);
